6862683abf94ff1f0e70c84707456a38e4639f1e
thepeg <marco.pegoraro@gmail.com>
2014-07-03 16:13:26 +0200

    fisrt commit

---
+++ index.js
@@ -0,0 +1,43 @@
+/**
+ * ACE Editor Custom Binding
+ * =========================
+ *
+ * turn any tag into an ACE editor instance and bind the source to a ko.observable
+ *
+ *     <div data-bind="ace:sourceField"></div>
+ *
+ * @TODO: add configuration support via allBindings or a configurable binding object
+ *
+ */
+
+ko.bindingHandlers.ace = {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        // copose the editor instance
+        var editor = ace.edit(element);
+        ko.utils.domData.set(element, 'ace-editor', editor);
+        
+        // editor configuration
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/javascript");
+        
+        // initial value
+        editor.setValue(ko.unwrap(valueAccessor()));
+        editor.gotoLine(0);
+        
+        // get updates
+        var sub = editor.getSession().on('change', function() {
+            if (ko.isWriteableObservable(valueAccessor())) {
+                valueAccessor()(editor.getValue());
+            }
+        });
+        
+        // clean up on DOM dispose
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            editor.destroy();
+        });
+    },
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var editor = ko.utils.domData.get(element, 'ace-editor');
+        editor.setValue(ko.unwrap(valueAccessor()), { silent: true });
+    }
+};
---
+++ readme.md
@@ -0,0 +1,10 @@
+ACE Editor Custom Binding
+=========================
+
+turn any tag into an ACE editor instance and bind the source to a ko.observable
+ 
+    <div data-bind="ace:sourceField"></div>
+ 
+@TODO: add configuration support via allBindings or a configurable binding object
+@TODO: evaluate the opportunity to add a "brace" custom binding when it comes to
+bundle ACE withing the app (no web workers).
